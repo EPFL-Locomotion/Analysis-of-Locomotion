e0fab1e505bc3661ae35b55e28a03ea270628841
Marco Milano <marco.milano@epfl.ch>
2018-05-02 13:40:08 +0200

    Filtering second attempt
    Same as before, but adding external for loop for each trial

--- preprocessing.m
+++ preprocessing.m
@@ -8,32 +8,38 @@ addpath(genpath('Healthy Recordings\Subject6\NO_FLOAT'));
 
 load('S6_NO_FLOAT.mat');
 
+Trials=fieldnames(S6_NO_FLOAT);
+Trials=Trials(9:11,1);
 EMGSensors=fieldnames(S6_NO_FLOAT.T_01.Raw.EMG);
 
-for idx=1:length(EMGSensors)
-    SensorName=EMGSensors{idx};
-    if isstruct(S6_NO_FLOAT.T_01.Raw.EMG)
-        %4-th order band pass filter (10-499 Hz)
-        [bp1,bp2]=butter(2,[10 499]/(S6_NO_FLOAT.T_01.fsEMG/2)); 
-        S6_NO_FLOAT.T_01.Filtered.(SensorName)=filter(bp1,bp2,S6_NO_FLOAT.T_01.Raw.EMG.(SensorName));
+for Trialidx=1:length(Trials)
+   TrialName=Trials{Trialidx}; 
+   if isstruct(S6_NO_FLOAT)
+    for Sensoridx=1:length(EMGSensors)
+        SensorName=EMGSensors{Sensoridx};
+        if isstruct(S6_NO_FLOAT.(TrialName).Raw.EMG)
+            %4-th order band pass filter (10-499 Hz)
+            [bp1,bp2]=butter(2,[10 499]/(S6_NO_FLOAT.(TrialName).fsEMG/2)); 
+            S6_NO_FLOAT.(TrialName).Filtered.(SensorName)=filter(bp1,bp2,S6_NO_FLOAT.(TrialName).Raw.EMG.(SensorName));
         
-        %rectification
-        S6_NO_FLOAT.T_01.Rectified.(SensorName)=abs(S6_NO_FLOAT.T_01.Filtered.(SensorName));
+            %rectification
+            S6_NO_FLOAT.(TrialName).Rectified.(SensorName)=abs(S6_NO_FLOAT.(TrialName).Filtered.(SensorName));
 
-        %4-th order high-pass filter (30 Hz)
-        [hp1,hp2]=butter(4,30/(S6_NO_FLOAT.T_01.fsEMG/2),'high'); 
-        S6_NO_FLOAT.T_01.Filtered2.(SensorName)=filter(hp1,hp2,S6_NO_FLOAT.T_01.Rectified.(SensorName));
+            %4-th order high-pass filter (30 Hz)
+            [hp1,hp2]=butter(4,30/(S6_NO_FLOAT.(TrialName).fsEMG/2),'high'); 
+            S6_NO_FLOAT.(TrialName).Filtered2.(SensorName)=filter(hp1,hp2,S6_NO_FLOAT.(TrialName).Rectified.(SensorName));
         
-        %4-th order Notch filter (50 Hz)
-        [n1,n2]=butter(4,[49.8 50.2]/(S6_NO_FLOAT.T_01.fsEMG/2),'stop'); 
-        S6_NO_FLOAT.T_01.Filtered3.(SensorName)=filter(n1,n2,S6_NO_FLOAT.T_01.Filtered2.(SensorName));
+            %4-th order Notch filter (50 Hz)
+            [n1,n2]=butter(4,[49.8 50.2]/(S6_NO_FLOAT.(TrialName).fsEMG/2),'stop'); 
+            S6_NO_FLOAT.(TrialName).Filtered3.(SensorName)=filter(n1,n2,S6_NO_FLOAT.(TrialName).Filtered2.(SensorName));
         
-        %4-th order low-pass filter (10 Hz)
-        [lp1,lp2]=butter(4,10/(S6_NO_FLOAT.T_01.fsEMG/2),'low'); 
-        S6_NO_FLOAT.T_01.Filtered4.(SensorName)=filter(lp1,lp2,S6_NO_FLOAT.T_01.Filtered3.(SensorName));
+            %4-th order low-pass filter (10 Hz)
+            [lp1,lp2]=butter(4,10/(S6_NO_FLOAT.(TrialName).fsEMG/2),'low'); 
+            S6_NO_FLOAT.(TrialName).Filtered4.(SensorName)=filter(lp1,lp2,S6_NO_FLOAT.(TrialName).Filtered3.(SensorName));
+        end
     end
+   end
 end
- 
 subplot(3,2,1)
 plot(S6_NO_FLOAT.T_01.Raw.EMG.LMG)
 subplot(3,2,2)
