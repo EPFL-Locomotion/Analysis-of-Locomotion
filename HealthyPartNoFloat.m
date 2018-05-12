e26d56c292ddba811f34e827a65005f17e7aabf5
Marco Milano <marco.milano@epfl.ch>
2018-05-12 18:39:15 +0200

    per Giammarco

--- HealthyPartNoFloat.m
+++ HealthyPartNoFloat.m
@@ -297,7 +297,158 @@ for n=1:2
     end
 end
 
+%% EMG parameters
+EMGType={'Filtered','Rectified','Filtered3','Filtered4'};
+EMGSensorsLeft={'LMG','LTA'};
+EMGSensorsRight={'RMG','RTA'};
 
+
+%divide into gait cycles - left
+for k=1:length(trials)
+     for j=1:length(PosIC_left.(trials{k}))-1
+        for l=1:length(EMGType)   
+            for i=1:length(EMGSensorsLeft)
+                S6_NO_FLOAT.(trials{k}).GaitCycles.(numbers{j}).EMG.(EMGType{l}).(EMGSensorsLeft{i})=S6_NO_FLOAT.(trials{k}).(EMGType{l}).(EMGSensorsLeft{i})((10*PosIC_left.(trials{k})(j)):(10*PosIC_left.(trials{k})(j+1)-1));
+            end                
+         end
+     end
+end
+for k=1:length(trials)
+     for j=1:length(PosIC_left.(trials{k}))-1
+         for i=1:length(EMGSensorsLeft)
+                S6_NO_FLOAT.(trials{k}).GaitCycles.(numbers{j}).EMG.Raw.(EMGSensorsLeft{i})=S6_NO_FLOAT.(trials{k}).Raw.EMG.(EMGSensorsLeft{i})((10*PosIC_left.(trials{k})(j)):(10*PosIC_left.(trials{k})(j+1)-1));
+         end
+     end
+end
+
+
+
+
+%divide into gait cycles - right
+for k=1:length(trials)
+     for j=1:length(PosIC_right.(trials{k}))-1
+        for l=1:length(EMGType)   
+            for i=1:length(EMGSensorsRight)
+                S6_NO_FLOAT.(trials{k}).GaitCycles.(numbers{j}).EMG.(EMGType{l}).(EMGSensorsRight{i})=S6_NO_FLOAT.(trials{k}).(EMGType{l}).(EMGSensorsRight{i})((10*PosIC_right.(trials{k})(j)):(10*PosIC_right.(trials{k})(j+1)-1));
+            end                
+         end
+     end
+end
+for k=1:length(trials)
+     for j=1:length(PosIC_right.(trials{k}))-1
+         for i=1:length(EMGSensorsRight)
+                S6_NO_FLOAT.(trials{k}).GaitCycles.(numbers{j}).EMG.Raw.(EMGSensorsRight{i})=S6_NO_FLOAT.(trials{k}).Raw.EMG.(EMGSensorsRight{i})((10*PosIC_right.(trials{k})(j)):(10*PosIC_right.(trials{k})(j+1)-1));
+         end
+     end
+end
+
+
+figure
+for k=1:length(trials)
+     for i=1:4
+         subplot(3,4,4*(k-1)+i)
+         plot(S6_NO_FLOAT.(trials{k}).Raw.EMG.(EMGSensors{i}));
+         title(sprintf('Trial %d - Sensor %s',k,EMGSensors{i}));
+     end
+end
+suptitle('Raw signal');
+
+
+figure
+for k=1:length(trials)
+     for i=1:4
+         subplot(3,4,4*(k-1)+i)
+         plot(S6_NO_FLOAT.(trials{k}).Filtered.(EMGSensors{i}));
+         title(sprintf('Trial %d - Sensor %s',k,EMGSensors{i}));
+     end
+end
+suptitle('Filtered signal');
+
+
+figure
+for k=1:length(trials)
+     for i=1:4
+         subplot(3,4,4*(k-1)+i)
+         plot(S6_NO_FLOAT.(trials{k}).Filtered4.(EMGSensors{i}));
+         title(sprintf('Trial %d - Sensor %s',k,EMGSensors{i}));
+     end
+end
+suptitle('Final filtered signal');
+
+
+%--
+figure
+     for j=2:3
+         for i=1:length(fieldnames(S6_NO_FLOAT.(trials{3}).GaitCycles))
+            subplot(4,4,4*(j-1)+i)
+            plot(S6_NO_FLOAT.(trials{3}).GaitCycles.(numbers{i}).EMG.Raw.(EMGSensors{j}));
+            title(sprintf('Gait cycle %d - Sensor %s',i,EMGSensors{j}));
+         end   
+     end
+suptitle('Trial 3 - Raw');
+
+
+figure
+     for j=2:3
+         for i=1:length(fieldnames(S6_NO_FLOAT.(trials{1}).GaitCycles))
+            subplot(4,4,4*(j-1)+i)
+            plot(S6_NO_FLOAT.(trials{1}).GaitCycles.(numbers{i}).EMG.Filtered4.(EMGSensors{j}));
+            title(sprintf('Gait cycle %d - Sensor %s',i,EMGSensors{j}));
+         end   
+     end
+suptitle('Trial 1 - Final filtered');
+
+
+%%Bursts calculation visually
+Bursts.Position.T_01.GaitCycles.One.LTA=[108 705];
+Bursts.Position.T_01.GaitCycles.Two.LTA=[80 750];
+Bursts.Position.T_01.GaitCycles.Three.LTA=[44 736];
+Bursts.Position.T_01.GaitCycles.Four.LTA=[90 761];
+
+Bursts.Position.T_01.GaitCycles.One.RMG=[80 897];
+Bursts.Position.T_01.GaitCycles.Two.RMG=[72 752];
+Bursts.Position.T_01.GaitCycles.Three.RMG=[150 750];
+Bursts.Position.T_01.GaitCycles.Four.RMG=[147 824];
+
+Bursts.Position.T_02.GaitCycles.One.LTA=[193 897];
+Bursts.Position.T_02.GaitCycles.Two.LTA=[146 700];
+Bursts.Position.T_02.GaitCycles.Three.LTA=[110 710];
+
+Bursts.Position.T_02.GaitCycles.One.RMG=[86 750];
+Bursts.Position.T_02.GaitCycles.Two.RMG=[100 711];
+Bursts.Position.T_02.GaitCycles.Three.RMG=[94 700];
+
+
+Bursts.Position.T_03.GaitCycles.One.LTA=[390 1805];
+Bursts.Position.T_03.GaitCycles.Two.LTA=[260 1773];
+Bursts.Position.T_03.GaitCycles.Three.LTA=[260 1773];
+
+Bursts.Position.T_03.GaitCycles.One.RMG=[535 1900];
+Bursts.Position.T_03.GaitCycles.Two.RMG=[458 1805];
+Bursts.Position.T_03.GaitCycles.Three.RMG=[260 1773];
+
+%%Calculation of EMG parameters
+for i=1:3
+    for j=1:length(fieldnames(S6_NO_FLOAT.(trials{i}).GaitCycles))
+        for k=2:3
+            %Each burst falls between two gait cycles, i.e. each gait
+            %cycles has two half-bursts (one at the beginning and one at
+            %the end)
+            end1burst=Bursts.Position.(trials{i}).GaitCycles.(numbers{j}).(EMGSensors{k})(1);%end of the first half-burst
+            start2burst=Bursts.Position.(trials{i}).GaitCycles.(numbers{j}).(EMGSensors{k})(2);%start of the second half-burst
+            lengthburst=length(S6_NO_FLOAT.(trials{i}).GaitCycles.(numbers{j}).EMG.Raw.(EMGSensors{k}));
+        
+            %Duration in seconds (i.e. divided by 1000), i.e. how much of the gait cycle is burst
+            Bursts.Duration.(trials{i}).GaitCycles.(numbers{j}).(EMGSensors{k})=0.001*(end1burst-1+lengthburst-start2burst);
+        
+            %Max value of the burst (in the final filtered signal) in the gait cycle
+            Bursts.MaxValue.(trials{i}).GaitCycles.(numbers{j}).(EMGSensors{k})=max(S6_NO_FLOAT.(trials{i}).GaitCycles.(numbers{j}).EMG.Filtered4.(EMGSensors{k}));
+            
+            %Mean value of the burst (in the final filtered signal) in the gait cycle
+            Bursts.MeanValue.(trials{i}).GaitCycles.(numbers{j}).(EMGSensors{k})=mean(S6_NO_FLOAT.(trials{i}).GaitCycles.(numbers{j}).EMG.Filtered4.(EMGSensors{k})([1:end1burst,start2burst:lengthburst]));
+        end
+    end
+end
 
 
 
